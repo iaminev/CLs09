df197e1fb10842ed0b45e9907371f3667c090140
Ilshat Aminev <i.aminev@gmail.com>
2023-05-23 22:56:17 +0300

    Homework lesson 9

---
+++ Task 66/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15-> 120
+M = 4; N = 8. -> 30*/
+
+int SumOfElements(int m, int n)
+{
+    if (m == n) return n;
+    return m + SumOfElements(m + 1, n);
+}
+
+System.Console.WriteLine(SumOfElements(1, 15));
+System.Console.WriteLine(SumOfElements(4, 8));
---
+++ Task 68/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3->A(m, n) = 9
+m = 3, n = 2->A(m, n) = 29*/
+
+int Ack(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return Ack(m - 1, 1);
+    return Ack(m - 1, Ack(m, n - 1));
+}
+
+for (int m = 0; m <= 3; m++)
+{
+    for (int n = 0; n <= 10; n++)
+    {
+        System.Console.Write($"A({m}, {n}) = {Ack(m, n),4:F0},    ");
+    }
+    System.Console.WriteLine();
+}
